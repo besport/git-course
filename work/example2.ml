4fce1fb09fadc951faf758d5802a5a9e9f1f0e63
William <william@undefined.re>
2021-04-06 09:29:27 +0200

    [code] calc answer (not working?)

--- work/example2.ml
+++ work/example2.ml
@@ -1,2 +1,7 @@
 let _ =
-  print_endline "hello universe"
+  print_endline "hello universe";
+  let i = 30
+  and j = 2 in
+  let answer = i + j in
+  print_int answer;
+  print_endline " is the answer"
