2835a9c40440c08a1ab136cb93c00c07314100f9
William <william@undefined.re>
2021-04-06 09:32:51 +0200

    [remove me] add debug

--- work/example2.ml
+++ work/example2.ml
@@ -2,6 +2,12 @@ let _ =
   print_endline "hello universe";
   let i = 30
   and j = 2 in
+  print_int i;
+  print_string " - ";
+  print_int j;
+  print_string " - ";
+  print_int (i + j);
+  print_string " - ";
   let answer = i + j in
   print_int answer;
   print_endline " is the answer"
